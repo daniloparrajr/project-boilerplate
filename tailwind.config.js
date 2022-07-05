b74d5d7ee52737d2be1b652d059341b6d4a78d5a
Danilo Parra Jr <daniloparrajr.work@gmail.com>
2022-07-05 10:44:00 +0800

    feat: add sample tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js}'],
-  theme: {
-    extend: {},
-  },
+	theme: {
+		screens: {
+			sm: '375px',
+			md: '768px',
+			lg: '992px',
+			xl: '1440px',
+		},
+		fontFamily: {
+			'sans': ['Be Vietnam Pro', ...defaultTheme.fontFamily.sans]
+		},
+		extend: {
+			colors: {
+				brightRed: 'hsl(12, 88%, 59%)',
+			}
+		},
+	},
   plugins: [],
 }
